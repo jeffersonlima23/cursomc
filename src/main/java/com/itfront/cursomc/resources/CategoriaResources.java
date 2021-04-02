d13018b29d3e8b08caaf12016a91a304746eed48
Jefferson Coelho Lima <jefferson.coelho23@gmail.com>
2021-04-02 11:47:17 -0300

    TESTANDO A PRIMEIRA CLASSE DE DOMINIO - Categoria

--- src/main/java/com/itfront/cursomc/resources/CategoriaResources.java
+++ src/main/java/com/itfront/cursomc/resources/CategoriaResources.java
@@ -1,12 +1,22 @@
 package com.itfront.cursomc.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.itfront.cursomc.domain.Categoria;
+
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResources {
+	
+	Categoria cat = new Categoria(1, "Informatica");
+	Categoria cat2 = new Categoria(2, "escritorio");
+	
+	List<Categoria> lista = new ArrayList<>();
 	
 	@RequestMapping(method=RequestMethod.GET)
 	public String listar() {
